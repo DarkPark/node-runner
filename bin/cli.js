7a3e3a86fb220d441c57978ca5f78ea293964df3
Stanislav Kalashnik <s.kalashnik@infomir.com>
2017-12-06 12:55:31 +0200

    add files

---
+++ bin/cli.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+/**
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+var path    = require('path'),
+    program = require('commander'),
+    debug   = require('debug')('cli'),
+    runner  = require('../lib/main'),
+    pkgData = require('../package.json');
+
+
+program
+    .version(pkgData.version)
+    .usage('[options] [<task>]')
+    .description(pkgData.description)
+    .option('-c, --config [file]', 'configuration file with tasks', 'runner.js')
+    .option('-s, --serial', 'run all given tasks sequentially (instead of in parallel)');
+
+program.on('--help', function () {
+    console.log('\n  Examples:');
+    console.log('');
+    console.log('    $ @TODO');
+    console.log('');
+});
+
+program.parse(process.argv);
+
+// config absolute path
+program.config = path.normalize(path.join(process.cwd(), program.config));
+
+// load config
+try {
+    require(program.config);
+    runner.log.info('config file: ' + runner.log.colors.green(program.config));
+} catch ( error ) {
+    runner.log.fail('config file %s was not found!', runner.log.colors.bold(program.config));
+}
+
+// run
+if ( program.args.length === 1 ) {
+    // just a single task
+    runner.run(program.args[0]);
+} else {
+    if ( program.args.length > 1 ) {
+        // list of tasks
+        runner.log.info('run mode: ' + (program.serial ? 'serial' : 'parallel'));
+
+        if ( program.serial ) {
+            runner.task('default', runner.serial.apply(runner, program.args));
+        } else {
+            runner.task('default', runner.parallel.apply(runner, program.args));
+        }
+    }
+
+    runner.start();
+}
---
+++ readme.md
@@ -0,0 +1,59 @@
+Task runner
+===========
+
+[![build status](https://img.shields.io/travis/DarkPark/node-runner.svg?style=flat-square)](https://travis-ci.org/DarkPark/node-runner)
+[![npm version](https://img.shields.io/npm/v/node-runner.svg?style=flat-square)](https://www.npmjs.com/package/node-runner)
+[![dependencies status](https://img.shields.io/david/DarkPark/node-runner.svg?style=flat-square)](https://david-dm.org/DarkPark/node-runner)
+[![devDependencies status](https://img.shields.io/david/dev/DarkPark/node-runner.svg?style=flat-square)](https://david-dm.org/DarkPark/node-runner?type=dev)
+[![Gitter](https://img.shields.io/badge/gitter-join%20chat-blue.svg?style=flat-square)](https://gitter.im/DarkPark/spasdk)
+
+
+## Installation ##
+
+```bash
+npm install node-runner
+```
+
+
+## Usage ##
+
+Add to the scope:
+
+```js
+var runner = require('node-runner');
+```
+
+
+### Tasks configuration ###
+
+@todo
+
+
+### Logging ###
+
+General output:
+
+```js
+// 16:25:30.811 simple line
+runner.log.info('simple line');
+```
+
+Some task specific output:
+
+```js
+var log = runner.log.wrap('webpack');
+
+// 16:25:30.811 [webpack] build is ok
+log.info('build is ok');
+```
+
+
+## Contribution ##
+
+If you have any problems or suggestions please open an [issue](https://github.com/DarkPark/node-runner/issues)
+according to the contribution [rules](.github/contributing.md).
+
+
+## License ##
+
+`node-runner` is released under the [GPL-3.0 License](http://opensource.org/licenses/GPL-3.0).
